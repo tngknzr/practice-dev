f3cecd8dd013387bc56746788615b93bacac6619
Agustin <agustinparedes95@gmail.com>
2024-10-01 15:51:45 -0700

    practicing

---
+++ practice-dev/app/@profile/page.tsx
@@ -0,0 +1,8 @@
+export default function Profile() {
+    return(
+        <main>
+            <h1>Profile</h1>
+            <p>Profile page</p>
+        </main>
+    )
+}
---
+++ practice-dev/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+ 
+  return (
+      <main>
+        <h1>Welcome to my Page</h1>
+        
+      </main>
+  );
+}
